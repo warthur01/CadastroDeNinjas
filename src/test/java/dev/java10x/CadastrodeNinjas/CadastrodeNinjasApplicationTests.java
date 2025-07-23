303168a4c3524d4872be51b34c3aedec13efcd99
Arthur <arthurdsteixeira@gmail.com>
2025-07-23 11:39:28 -0300

    first commit

---
+++ src/test/java/dev/java10x/CadastrodeNinjas/CadastrodeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastrodeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
